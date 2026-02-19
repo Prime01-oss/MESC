743d01fb099a5d2fcc0ad0a424064a527f62cc53
Prime01-oss <patarsamar123abc@gmail.com>
2026-02-19 20:15:06 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+﻿import sys
+from PySide6.QtWidgets import QApplication
+from app_platform.ui_shell.app_shell import StemShell
+
+# --- Imports ---
+from ides.mathex.ide_plugin import MatlabTool
+from ides.mathematica.ide_plugin import MathematicaTool
+
+def run():
+    app = QApplication(sys.argv)
+    
+    shell = StemShell()
+    
+    # --- Register Tools ---
+    shell.register_tool(MatlabTool())
+    shell.register_tool(MathematicaTool())
+    
+    shell.switch_tool(0)
+    shell.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    run()
